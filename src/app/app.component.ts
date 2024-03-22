b4069ee5aa99bad7e7ff724ddf2ddd027eef4158
Jonathan J. Wall <101894118+Wallofsouls@users.noreply.github.com>
2024-03-22 14:40:58 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+interface Entrada {
+  titulo: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo1 = 'Registro de Usuarios';
+  mensaje = "";
+  existe = false;
+  nombre: string = "";
+  apellido: string = "";
+  cargo: string = "";
+  entradas: Entrada[];
+
+  constructor() {
+    this.entradas = [
+      { titulo: "World 2 Meet" },
+      { titulo: "Iberostar" },
+      { titulo: "Jet2" },
+      { titulo: "NewBlue" },
+      { titulo: "Kannak" },
+    ]
+  }
+
+  registrarUsuario() {
+    this.existe = true
+    this.mensaje = "Usuario Registrado con éxito"
+  }
 }
+
+
